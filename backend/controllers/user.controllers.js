c8c001dd03e433371c96a71a989d70e41226d079
Jimmy Clairy <jimcl@hotmail.fr>
2022-09-29 18:58:32 +0200

    create routes user (login get del put)

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,7 +1,8 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const UserModel = require('../models/user.models');
 
-const admin = {
+const Admin = {
     pseudo: "Admin",
     email: "admin@test.fr",
     password: "Admin123"
@@ -11,7 +12,7 @@ exports.signup = (req, res) => {
     let verifAdmin = false;
     let messageUser = "Utilisateur";
 
-    if (req.body.pseudo === admin.pseudo && req.body.email === admin.email && req.body.password === admin.password) {
+    if (req.body.pseudo === Admin.pseudo && req.body.email === Admin.email && req.body.password === Admin.password) {
         verifAdmin = true;
         messageUser = "Administrateur"
     }
@@ -29,4 +30,65 @@ exports.signup = (req, res) => {
                 .catch(error => res.status(400).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
+};
+
+exports.login = (req, res) => {
+    UserModel.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' });
+            }
+            // bcrypt compare le mot de passe entré par l'utilisateur avec le hash enregistré dans la base de données
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' });
+                    } else {
+                        res.status(200).json({
+                            userId: user._id,
+                            // Fonction sign() permet de chiffrer un nouveau token
+                            token: jwt.sign(
+                                { userId: user._id },
+                                process.env.TOKEN_SECRET,
+                                { expiresIn: '24h' })
+                        });
+                    }
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+};
+
+exports.getOneUser = (req, res) => {
+    UserModel.findOne({ _id: req.params.id }).select('-password')
+        .then((user) => res.status(200).json(user))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.getAllUsers = async (req, res) => {
+    const users = await UserModel.find(/*{ admin: false }*/)/*.select('-password')*/
+    res.status(200).json(users)
+};
+
+exports.deleteOneUser = (req, res) => {
+    UserModel.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: "Utilisateur supprimé" }))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.modifyOneUser = async (req, res) => {
+
+    const user = {
+        pseudo: req.body.pseudo,
+        email: req.body.email,
+    };
+
+    if (req.body.password) {
+        user.password = await bcrypt.hash(req.body.password, 10)
+    };
+
+    UserModel.updateOne({ _id: req.params.id }, { ...user })
+        .then(() => res.status(200).json({ message: "Utilisateur modifié" }))
+        .catch(error => res.status(400).json({ error }));
+
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,6 +3,13 @@ const userCtrl = require('../controllers/user.controllers');
 
 // http://localhost:7000/api/user/
 router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
+
+router.get('/:id', userCtrl.getOneUser);
+router.get('/', userCtrl.getAllUsers);
+
+router.delete('/:id', userCtrl.deleteOneUser);
+router.put('/:id', userCtrl.modifyOneUser);
 
 
 module.exports = router;
