9df52c7d489d50d7918b365509df0d829c01cfc6
Jimmy Clairy <jimcl@hotmail.fr>
2022-09-30 16:52:51 +0200

    backend finish user.controllers

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const ObjectID = require("mongoose").Types.ObjectId;
 const UserModel = require('../models/user.models');
+const isEmail = require('validator/lib/isEmail');
 
 const Admin = {
     pseudo: "Admin",
@@ -19,13 +21,13 @@ exports.signup = (req, res) => {
 
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
-            const user = new UserModel({
+            const User = new UserModel({
                 pseudo: req.body.pseudo,
                 email: req.body.email,
                 password: hash,
                 admin: verifAdmin
             });
-            user.save()
+            User.save()
                 .then(() => res.status(201).json({ message: messageUser + ' créé !' }))
                 .catch(error => res.status(400).json({ error }));
         })
@@ -59,36 +61,75 @@ exports.login = (req, res) => {
         .catch(error => res.status(500).json({ error }));
 };
 
-exports.getOneUser = (req, res) => {
-    UserModel.findOne({ _id: req.params.id }).select('-password')
+exports.getAllUsers = (req, res) => {
+
+
+    UserModel.find(/*{ admin: false }*/).select('-password')
         .then((user) => res.status(200).json(user))
         .catch(error => res.status(400).json({ error }));
 };
 
-exports.getAllUsers = async (req, res) => {
-    const users = await UserModel.find(/*{ admin: false }*/)/*.select('-password')*/
-    res.status(200).json(users)
+exports.getOneUser = (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send("ID inconnu : " + req.params.id);
+
+    UserModel.findById(req.params.id).select('-password')
+        .then((user) => res.status(200).json(user))
+        .catch(error => res.status(400).json({ error }));
 };
 
 exports.deleteOneUser = (req, res) => {
-    UserModel.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: "Utilisateur supprimé" }))
-        .catch(error => res.status(400).json({ error }));
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send("ID inconnu : " + req.params.id);
+
+    UserModel.findById(req.params.id)
+        .then((user) => {
+            if (req.auth.userId === user.id) {
+
+                UserModel.findByIdAndDelete(req.params.id)
+                    .then(() => res.status(200).json({ message: "Utilisateur supprimé" }))
+                    .catch(error => res.status(400).json({ error }));
+
+            } else {
+                res.status(401).json({ message: 'Non autoriser' });
+            }
+        })
 };
 
 exports.modifyOneUser = async (req, res) => {
+    try {
+        if (!ObjectID.isValid(req.params.id))
+            return res.status(400).send("ID inconnu : " + req.params.id);
 
-    const user = {
-        pseudo: req.body.pseudo,
-        email: req.body.email,
-    };
+        if (req.body.email) {
+            if (!isEmail(req.body.email))
+                return res.status(400).send({ message: "Email non conforme" });
+        }
 
-    if (req.body.password) {
-        user.password = await bcrypt.hash(req.body.password, 10)
-    };
+        const User = {
+            pseudo: req.body.pseudo,
+            email: req.body.email,
+        };
 
-    UserModel.updateOne({ _id: req.params.id }, { ...user })
-        .then(() => res.status(200).json({ message: "Utilisateur modifié" }))
-        .catch(error => res.status(400).json({ error }));
+        if (req.body.password) {
+            User.password = await bcrypt.hash(req.body.password, 10)
+        };
 
+        UserModel.findById(req.params.id)
+            .then((user) => {
+
+                if (req.auth.userId === user.id) {
+
+                    UserModel.findByIdAndUpdate(req.params.id, { ...User })
+                        .then(() => res.status(200).json({ message: "Utilisateur modifié" }))
+                        .catch(error => res.status(400).json({ error }));
+
+                } else {
+                    res.status(401).json({ message: 'Non autoriser' });
+                }
+            })
+
+    } catch (error) {
+        res.status(401).json({ error });
+    }
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
 const userCtrl = require('../controllers/user.controllers');
+const auth = require('../middleware/auth')
 
 // http://localhost:7000/api/user/
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
-router.get('/:id', userCtrl.getOneUser);
-router.get('/', userCtrl.getAllUsers);
+router.get('/:id', auth, userCtrl.getOneUser);
+router.get('/', auth, userCtrl.getAllUsers);
 
-router.delete('/:id', userCtrl.deleteOneUser);
-router.put('/:id', userCtrl.modifyOneUser);
+router.delete('/:id', auth, userCtrl.deleteOneUser);
+router.put('/:id', auth, userCtrl.modifyOneUser);
 
 
 module.exports = router;
