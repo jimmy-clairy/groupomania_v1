067806fc7fa4ad0b8577c420463d27166f9345a2
Jimmy Clairy <jimcl@hotmail.fr>
2022-10-05 19:12:01 +0200

    end controllers post

--- backend/controllers/post.controllers.js
+++ backend/controllers/post.controllers.js
@@ -1,6 +1,8 @@
 const PostModel = require('../models/post.models');
+const ObjectID = require("mongoose").Types.ObjectId;
+const fs = require('fs');
 
-exports.createPost = (req, res, next) => {
+exports.createPost = (req, res) => {
 
     const post = new PostModel({
         posterId: req.auth.userId,
@@ -11,12 +13,80 @@ exports.createPost = (req, res, next) => {
     if (req.file) { post.imageUrl = `${req.protocol}://${req.get('host')}/images/${req.file.filename}` }
 
     post.save()
-        .then(() => { res.status(201).json({ message: 'Post enregistré !' }) })
+        .then(() => { res.status(201).json({ message: 'Saved post !' }) })
         .catch(error => { res.status(400).json({ error }) })
 };
 
+exports.getOnePost = (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send("ID inconnu : " + req.params.id);
+
+    PostModel.findById(req.params.id)
+        .then((post) => { res.status(201).json(post) })
+        .catch(error => { res.status(400).json({ error }) })
+};
+
+exports.getAllPosts = (req, res) => {
+    PostModel.find()
+        .then((posts) => { res.status(201).json(posts) })
+        .catch(error => { res.status(400).json({ error }) })
+};
+
+exports.deleteOnePost = (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send("ID inconnu : " + req.params.id);
+
+    PostModel.findById(req.params.id)
+        .then((post) => {
+            if (post.posterId === req.auth.userId) {
+                // Supprime l'image
+                const filename = post.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    PostModel.findByIdAndDelete(req.params.id)
+                        .then(() => { res.status(200).json({ message: 'Delete post !' }) })
+                        .catch(error => res.status(401).json({ error }));
+                });
+            } else {
+                return res.status(201).json({ message: 'No authorize !' })
+            }
+        })
+        .catch(error => { res.status(400).json({ error }) })
+};
+
+exports.modifyOnePost = (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send("ID inconnu : " + req.params.id);
+
+    PostModel.findById(req.params.id)
+        .then((post) => {
+            if (post.posterId === req.auth.userId) {
+
+                const postModify = {
+                    post: req.body.post,
+                };
+
+                if (req.file) {
+                    const filename = post.imageUrl.split('/images/')[1];
+                    fs.unlink(`images/${filename}`, (err) => {
+                        if (err) throw err;
+                        console.log(`images/${filename} was deleted`);
+                    });
+                    postModify.imageUrl = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+                };
+
+                PostModel.findByIdAndUpdate(req.params.id, postModify)
+                    .then(() => { res.status(201).json({ message: 'Modify one post !' }) })
+                    .catch(error => { res.status(400).json({ error }) })
+            } else {
+                return res.status(201).json({ message: 'No authorize !' })
+            }
+        })
+        .catch(error => { res.status(400).json({ error }) })
+};
+
+// Deleted
 exports.deleteAllPosts = (req, res) => {
     PostModel.deleteMany()
-        .then(() => { res.status(201).json({ message: 'All Post deleted !' }) })
+        .then(() => { res.status(201).json({ message: 'Delete all posts !' }) })
         .catch(error => { res.status(400).json({ error }) })
-}
+};
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -5,7 +5,10 @@ const postCtrl = require('../controllers/post.controllers');
 
 // http://localhost:7000/api/post/
 router.post('/', auth, multer, postCtrl.createPost);
+router.get('/', auth, postCtrl.getAllPosts);
+router.get('/:id', auth, postCtrl.getOnePost);
 router.delete('/', auth, postCtrl.deleteAllPosts);
-
+router.delete('/:id', auth, postCtrl.deleteOnePost);
+router.put('/:id', auth, multer, postCtrl.modifyOnePost);
 
 module.exports = router;
